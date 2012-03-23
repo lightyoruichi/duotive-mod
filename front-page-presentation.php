499901a54ebca48c488ccd7bf702444180c08d1d
Harinder Singh <lightyoruichi@ktard.co.cc>
2012-03-23 22:26:11 +0800

    the modded version

--- front-page-presentation.php
+++ front-page-presentation.php
@@ -16,10 +16,18 @@
                 <?php if ( get_option('fppre_intro_text') != '' ): ?>
                 	<p><?php echo get_option('fppre_intro_text'); ?></p>
                 <?php endif; ?>
+                             <img style="float: right; margin-top:-74px;" src="http://beta.tandemic.com/images/people2.png" />
+
                 <div class="sep">&nbsp;</div>
+                 <div style="float:right; margin-top: -20px;">
+                    <a href="/yb-hoo-song-chang/" class="more-link">YB Hoo<br> Seong<br> Chang</a>
+                    <a href="/dato-seri-hishammuddin-tun-hussein/" class="more-link">Dato' Sri<br> Hishammuddin<br> Tun Hussein</a>
+                    <a href="/yb-encik-vidyananthan-al-ramanadhan/" class="more-link">YB Encik<br> Vidyananthan<br> Ramanadhan</a>
+                 </div>
             <!-- end of intro -->
             </div>
 		<?php endif; ?>      
+        <h3 class="dshh">Dato Seri' Hishammuddin Hussein</h3>
 		<?php if ( get_option('fppre_row1') == '') $fppre_row1 = 'yes'; else $fppre_row1 = get_option('fppre_row1');?>    
         <?php if ( $fppre_row1 == 'yes' ): ?>
             <div id="front-page-presentation-row-1" class="clearfix">
@@ -70,7 +78,7 @@
                                 <?php the_title(); ?>
                             </a>
                         </h6>                                            
-                        <?php global $more; $more = 0; the_content(__('Read More &#187;','duotive')); ?>
+                        <?php global $more; $more = 0; the_excerpt(__('Read More &#187;','duotive')); ?>
                     </div>
                     <?php if ( $i%4 == 0 ) echo '<div class="post-sep"></div>'; ?>
                     <?php $i++; ?>                        
